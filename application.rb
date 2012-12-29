c92cd042a6f53ace1ea20e785324929af75e0259
twer <twer@localhost.(none)>
2012-12-29 22:28:36 +0800

    jtong - change line to fix create_file missed bug

--- application.rb
+++ application.rb
@@ -3,11 +3,9 @@ gem 'haml-rails'
 
 gem 'jt-mobile-kit-rails'
 
-create_file ".rvmrc", "#!/bin/sh\nrvm use ruby-1.9.3-p194@rails"
+run 'bundle install'
 
-inside app_name do
-  run 'bundle install'
-end
+create_file ".rvmrc", "#!/bin/sh\nrvm use ruby-1.9.3-p194@rails"
 
 generate("jt:new")
 
